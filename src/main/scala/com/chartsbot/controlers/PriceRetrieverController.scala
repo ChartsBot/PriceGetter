8d95537251836bac79095838049420b5f169e947
ben <rottedben@protonmail.com>
2022-01-20 15:50:35 +0100

    Only applying new strategy when worth it (more than 5 calls)

--- src/main/scala/com/chartsbot/controlers/PriceRetrieverController.scala
+++ src/main/scala/com/chartsbot/controlers/PriceRetrieverController.scala
@@ -24,36 +24,39 @@ class DefaultPriceRetrieverController @Inject() (oracleDAO: OracleDAO, sqlBlocks
 
   override def handleTimestampBasedRequest(timestamps: List[Int], token: String)(chain: SupportedChains): Future[List[Either[String, PriceAtTimestamp]]] = {
 
-    val lowerBoundTs = timestamps.min
-    val upperBoundTs = timestamps.max
-    val fRows = sqlBlocksDAO.getRangeOfTs(from = lowerBoundTs, to = upperBoundTs)(chain)
-    val r = fRows map {
-      case Left(value) =>
-        val a = Future.successful(List(Left(s"Sql Error getting ts range for timestamp range $lowerBoundTs - $upperBoundTs: ${value.errorMessage}.")))
-        a
-      case Right(sqlBlocks) =>
-        val r: List[Future[Right[Nothing, PriceAtTimestamp]]] = for (ts <- timestamps) yield {
-          val closestBlock = sqlBlocks.minBy(p => math.abs(p.blockTimestamp - ts))
-          logger.debug(s"Getting data for block: ${closestBlock.number} at ts ${closestBlock.blockTimestamp}")
-          oracleDAO.getPriceAsync(token, closestBlock.number)(chain).map(p => Right(PriceAtTimestamp(p.blockNumb, closestBlock.blockTimestamp, p.price)))
+    if (timestamps.size >= 5) {
+      val lowerBoundTs = timestamps.min
+      val upperBoundTs = timestamps.max
+      val fRows = sqlBlocksDAO.getRangeOfTs(from = lowerBoundTs - 1, to = upperBoundTs + 1)(chain)
+      val r = fRows map {
+        case Left(value) =>
+          val a = Future.successful(List(Left(s"Sql Error getting ts range for timestamp range $lowerBoundTs - $upperBoundTs: ${value.errorMessage}.")))
+          a
+        case Right(sqlBlocks) =>
+          val r: List[Future[Right[Nothing, PriceAtTimestamp]]] = for (ts <- timestamps) yield {
+            val closestBlock = sqlBlocks.minBy(p => math.abs(p.blockTimestamp - ts))
+            logger.debug(s"Getting data for block: ${closestBlock.number} at ts ${ts}")
+            oracleDAO.getPriceAsync(token, closestBlock.number)(chain).map(p => Right(PriceAtTimestamp(p.blockNumb, ts, p.price)))
+          }
+          Future.sequence(r)
+      }
+      r.flatten
+    } else {
+
+      val a = for (timestamp <- timestamps) yield {
+        val r = sqlBlocksDAO.getClosest(timestamp)(chain).map {
+          case Left(value) =>
+            Future.successful(Left(s"Error getting closest block for timestamp ${timestamp}: ${value.errorMessage}."))
+          case Right(block) =>
+            logger.debug(s"Getting data for block: ${block.head.number} at ts ${block.head.blockTimestamp}")
+            oracleDAO.getPriceAsync(token, block.head.number)(chain).map(p => Right(PriceAtTimestamp(p.blockNumb, timestamp, p.price)))
         }
-        Future.sequence(r)
+        r.flatten
+
+      }
+
+      Future.sequence(a)
     }
-    r.flatten
-    //
-    //    val a = for (timestamp <- timestamps) yield {
-    //      val r = sqlBlocksDAO.getClosest(timestamp)(chain).map {
-    //        case Left(value) =>
-    //          Future.successful(Left(s"Error getting closest block for timestamp ${timestamp}: ${value.errorMessage}."))
-    //        case Right(block) =>
-    //          logger.debug(s"Getting data for block: ${block.head.number} at ts ${block.head.blockTimestamp}")
-    //          oracleDAO.getPriceAsync(token, block.head.number)(chain).map(p => Right(PriceAtTimestamp(p.blockNumb, timestamp, p.price)))
-    //      }
-    //      r.flatten
-    //
-    //    }
-    //
-    //    Future.sequence(a)
 
   }
 
